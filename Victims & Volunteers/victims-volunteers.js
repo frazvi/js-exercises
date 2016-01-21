81208ac0c89866424988a7e7830684cce5a46f6e
frazvi <fatimarazvi@gmail.com>
2016-01-21 15:26:44 -0700

    victims & volunteers

--- Victims & Volunteers/victims-volunteers.js
+++ Victims & Volunteers/victims-volunteers.js
@@ -1,11 +1,38 @@
+var victimName = ['Victim Names: '];
+var victimPhoneNumber = ['Victim Phone Numbers: '];
+var victimStreet = ['Victim Street Names: '];
+var volunteerName = ['Volunteer Names: '];
+var volunteerPhoneNumber = ['Volunteer Phone Numbers: '];
+var volunteerStreet = ['Volunteer Street Names: '];
+
+
 var numberDisasterVictims = prompt("How many disaster victims will you be entering?")
 for (i=0; i<numberDisasterVictims; i++) {
-	prompt("Please enter a name, phone number, and street for the victim.")
+	var vName = prompt("Please enter a name for the victim.");
+	var vNumber = prompt("Please enter a phone number for the victim.");
+	var vStreet = prompt("Please enter a street name for the victim.");
+	victimName.push(vName);
+	victimPhoneNumber.push(vNumber);
+	victimStreet.push(vStreet);
 }
 
+console.log(victimName);
+console.log(victimPhoneNumber);
+console.log(victimStreet);
 
+var numberDisasterVolunteers = prompt("How many volunteers will you be entereing?")
+for (i=0; i<numberDisasterVolunteers; i++) {
+	var voName = prompt("Please enter a name for the volunteer.");
+	var voNumber = prompt("Please enter a number for the volunteer.");
+	var voStreet = prompt("Please enter a street name for the volunteer.");
+	volunteerName.push(voName);
+	volunteerPhoneNumber.push(voNumber);
+	volunteerStreet.push(voStreet);
+}
 
+console.log(volunteerName);
+console.log(volunteerPhoneNumber);
+console.log(volunteerStreet);
 
-// var victimName [];
-// var victimPhoneNumber [];
-// var victimStreet [];
+alert(victimName.join("\n") + ("\n") + victimPhoneNumber.join("\n") + ("\n") + victimStreet.join("\n") + ("\n") + volunteerName.join("\n") + ("\n") + volunteerPhoneNumber.join("\n") + ("\n") + volunteerStreet.join);
+
