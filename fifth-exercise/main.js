66d73b0b097896e41a4dd9f0ef487e23572d7ab2
frazvi <fatimarazvi@gmail.com>
2016-01-20 17:16:58 -0700

    simple validator

--- fifth-exercise/main.js
+++ fifth-exercise/main.js
@@ -3,27 +3,28 @@ var intro=prompt("Hello, and welcome! This website is going to ask you to enter 
 var phoneNumber = prompt("Please enter your phone number.");
 var splitPhoneNumber = phoneNumber.split('-');
 if (phoneNumber.charAt(3) === '-' && phoneNumber.charAt(7) === '-') {
-	for (i=0; i<splitPhoneNumber.length; i++)
+	for (var i=0; i<splitPhoneNumber.length; i++) {
 		if (isFinite(parseInt(splitPhoneNumber)[i])) {
 			var dateOfBirth=prompt("Please enter your date of birth.");
+
 			if (dateOfBirth.charAt(2) != '/' && dateOfBirth.charAt(5) != '/') {
 				var dateOfBirth= prompt("Sorry, it seems that your entry is invalid. Please enter your date of birth using only numbers and forward slashes, in the xx/xx/xx format.")
-			}
-			else {var postalCode=prompt("Please enter your postal code.")}
+			} else {var postalCode=prompt("Please enter your postal code.")}
+
 			if (postalCode.length != 5 && (postalCode.length != 10 && postalCode.charAt(5) != '-')) {
 				var postalCode=prompt("You have entered an invalid postal code. Please enter your postal code using only numbers and dashes. Your postal code must be in either the xxxxx, or the xxxxx-xxxx format.")
-			}
-			else {var stateAbbreviation=prompt("Please enter the state that you are from, using its abbreviation.")}
+			} else {var stateAbbreviation=prompt("Please enter the state that you are from, using its abbreviation.")}
+
 			if (stateAbbreviation.length > 2) {
 				var stateAbbreviation = prompt("Sorry! It looks like what you entered is invalid. Please enter a valid two-letter state abbreviation.")
-			}
-			else {var finalAnswer = prompt("You're almost done! Here is the final question: Are you married? Please entere yes or no.")}
+			} else {var finalAnswer = prompt("You're almost done! Here is the final question: Are you married? Please entere yes or no.")}
+
 			if (finalAnswer != 'yes' && finalAnswer != 'no') {
 				var finalQuestion = prompt("Sorry, you have entered an invalid answer. Please enter either yes or no.")
-			}
-			else {prompt("Thank you! We have saved your information and will be using it to steal your identity. Have a great day!")}
-		}
-		else {prompt("Oops! It looks like your entry is invalid. Please enter your phone number using only numbers and dashes, in the xxx-xxx-xxxx format.")}			
+			} else {prompt("Thank you! We have saved your information and will be using it to steal your identity. Have a great day!")}
+		} else {prompt("Oops! It looks like your entry is invalid. Please enter your phone number using only numbers and dashes, in the xxx-xxx-xxxx format.")}
+	} 	
+}			
 else {prompt("Oops! It looks like your entry is invalid. Please enter your phone number using only numbers and dashes, in the xxx-xxx-xxxx format.")}
 
 
