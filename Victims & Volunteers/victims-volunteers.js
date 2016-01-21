25e557d869456817fde815589a61a75268293dde
frazvi <fatimarazvi@gmail.com>
2016-01-20 17:18:53 -0700

    victims and volunteers

---
+++ Victims & Volunteers/victims-volunteers.js
@@ -0,0 +1,11 @@
+var numberDisasterVictims = prompt("How many disaster victims will you be entering?")
+for (i=0; i<numberDisasterVictims; i++) {
+	prompt("Please enter a name, phone number, and street for the victim.")
+}
+
+
+
+
+// var victimName [];
+// var victimPhoneNumber [];
+// var victimStreet [];
